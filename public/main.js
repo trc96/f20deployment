ef6e7dda7ef32f6e5c46990c99fd965eeff0d8e1
trc96 <toddcrandall1996@gmail.com>
2022-04-06 14:38:16 -0600

    h1

--- public/main.js
+++ public/main.js
@@ -1,1 +1,7 @@
 console.log('JS Connected')
+
+const presentAlert = () => {
+    alert(`I said I'm working on it...what did you expect to happen?`)
+}
+
+document.querySelector('button').addEventListener('button', presentAlert)
