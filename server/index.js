27a3963ad872bb42cb861f580042569c1086e80a
trc96 <toddcrandall1996@gmail.com>
2022-04-07 13:04:10 -0600

    added cors and installed rollbar

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.static('public'))
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: 'e370c411fa85493b90370984ee8d8385',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.log('Someone requested the home page of the website')
 })
 
 const PORT = process.env.PORT || 4005
